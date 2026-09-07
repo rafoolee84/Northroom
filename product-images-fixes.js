c686320eb1132831efba679cac39aebffcf0c36d
rafoolee84 <albazirafal859@gmail.com>
2026-09-07 00:52:50 -0400

    Add product-images-fixes.js for 3 duplicate hero swaps

---
+++ product-images-fixes.js
@@ -0,0 +1,5 @@
+window.NORTHROOM_IMAGES=Object.assign(window.NORTHROOM_IMAGES||{},{
+"Textured Bath Mat":"https://images.pexels.com/photos/19203526/pexels-photo-19203526.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"Linen Duvet Sham (Pair)":"https://images.unsplash.com/photo-1613721531731-3b7a7d96cb8a?w=1200&auto=format&fit=crop",
+"Woven Placemats (Set of 4)":"https://images.pexels.com/photos/13923045/pexels-photo-13923045.jpeg?auto=compress&cs=tinysrgb&w=1200"
+});
