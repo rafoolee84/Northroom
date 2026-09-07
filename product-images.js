5cc76718d4ab9afaec6dc06a5d3f887444a7600f
rafoolee84 <albazirafal859@gmail.com>
2026-09-07 00:27:48 -0400

    Replace PLACEHOLDER product-images.js with lamp hero map stub

--- product-images.js
+++ product-images.js
@@ -1,1 +1,3 @@
-PLACEHOLDER_REPLACE_WITH_FILE
+window.NORTHROOM_IMAGES=Object.assign(window.NORTHROOM_IMAGES||{},{
+"Matte Ceramic Lamp with Linen Shade":"https://images.pexels.com/photos/17158341/pexels-photo-17158341.jpeg?auto=compress&cs=tinysrgb&w=1200"
+});
