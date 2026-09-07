aba255e3ae20cf232633405e9a506bed6529062b
rafoolee84 <albazirafal859@gmail.com>
2026-09-07 00:46:54 -0400

    Restore category photos via prior Sourcer map + curtain/Mark overlays

--- product-images.js
+++ product-images.js
@@ -1,3 +1,17 @@
-window.NORTHROOM_IMAGES=Object.assign(window.NORTHROOM_IMAGES||{},{
-"Matte Ceramic Lamp with Linen Shade":"https://images.pexels.com/photos/17158341/pexels-photo-17158341.jpeg?auto=compress&cs=tinysrgb&w=1200"
-});
+(function(){
+  try {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://cdn.jsdelivr.net/gh/rafoolee84/Northroom@7e072cc033f563594ba8bfff6d91d203863af9a5/product-images.js", false);
+    xhr.send(null);
+    if (xhr.status === 200 && xhr.responseText) {
+      (0, eval)(xhr.responseText);
+    }
+  } catch (e) {}
+  window.NORTHROOM_IMAGES = Object.assign(window.NORTHROOM_IMAGES || {}, {
+    "Sheer Linen Curtain Panels (Pair)": "https://images.unsplash.com/photo-1764422097759-1fa737c50c51?w=800&q=80",
+    "Matte Ceramic Lamp with Linen Shade": "https://images.pexels.com/photos/17158341/pexels-photo-17158341.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "Northroom Ceramic Mug 11oz": "https://www.northroomhome.com/images/mug.jpg",
+    "Northroom Faux Suede Pillowcase": "https://www.northroomhome.com/images/pillow.jpg",
+    "Northroom Matte Poster 7x5": "https://www.northroomhome.com/images/poster.jpg"
+  });
+})();
