42a8f9c5b59fc38dd1cbb14cd42bebe97032a679
rafoolee84 <albazirafal859@gmail.com>
2026-09-07 01:21:07 -0400

    Manufacturer heroes: Natural Linen pillowcases, Diamond bath mat, Euro shams

--- product-images-fixes.js
+++ product-images-fixes.js
@@ -4,8 +4,11 @@ window.NORTHROOM_IMAGES=Object.assign(window.NORTHROOM_IMAGES||{},{
 "Heavyweight Linen Pillowcases (Pair)":"https://m.media-amazon.com/images/I/513X+pGE9rL._AC_SL1500_.jpg",
 "Washed Linen Euro Shams (Pair)":"https://m.media-amazon.com/images/I/61A4VrmVgdL._AC_SL1500_.jpg",
 "Linen Duvet Sham (Pair)":"https://m.media-amazon.com/images/I/61A4VrmVgdL._AC_SL1500_.jpg",
-"Chenille Textured Bath Mat":"https://ae-pic-a1.aliexpress-media.com/kf/Sbb7138728ed64f2da3133c0d408165fbw.jpg",
-"Textured Bath Mat":"https://ae-pic-a1.aliexpress-media.com/kf/Sbb7138728ed64f2da3133c0d408165fbw.jpg",
+"Diamond Textured Cotton Bath Mat":"https://m.media-amazon.com/images/I/8102qV28ytL._AC_SL1500_.jpg",
+"Textured Bath Mat":"https://m.media-amazon.com/images/I/8102qV28ytL._AC_SL1500_.jpg",
+"Chenille Textured Bath Mat":"https://m.media-amazon.com/images/I/8102qV28ytL._AC_SL1500_.jpg",
 "Woven Placemats (Set of 4)":"https://images.pexels.com/photos/13923045/pexels-photo-13923045.jpeg?auto=compress&cs=tinysrgb&w=1200",
-"Matte Black Candle Warmer":"https://images.pexels.com/photos/31236093/pexels-photo-31236093.jpeg?auto=compress&cs=tinysrgb&w=1200"
+"Matte Black Candle Warmer":"https://images.pexels.com/photos/31236093/pexels-photo-31236093.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"Ceramic Wall Light Fixture":"https://images.pexels.com/photos/18545884/pexels-photo-18545884.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"Frosted Glass Night Light":"https://images.pexels.com/photos/14380620/pexels-photo-14380620.jpeg?auto=compress&cs=tinysrgb&w=1200"
 });
