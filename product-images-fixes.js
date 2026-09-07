b3ca0ca33a6e8518addfec70356dedfa5c4810d8
rafoolee84 <albazirafal859@gmail.com>
2026-09-07 00:59:20 -0400

    Override cafe pendant + woman pillowcase with clear product heroes

--- product-images-fixes.js
+++ product-images-fixes.js
@@ -1,5 +1,7 @@
 window.NORTHROOM_IMAGES=Object.assign(window.NORTHROOM_IMAGES||{},{
-"Textured Bath Mat":"https://images.pexels.com/photos/19203526/pexels-photo-19203526.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"Ceramic Pendant Shade":"https://images.unsplash.com/photo-1763705859079-29c5cc0576bc?w=1200&auto=format&fit=crop",
+"Heavyweight Linen Pillowcases (Pair)":"https://images.pexels.com/photos/8325261/pexels-photo-8325261.jpeg?auto=compress&cs=tinysrgb&w=1200",
 "Linen Duvet Sham (Pair)":"https://images.unsplash.com/photo-1613721531731-3b7a7d96cb8a?w=1200&auto=format&fit=crop",
+"Textured Bath Mat":"https://images.pexels.com/photos/19203526/pexels-photo-19203526.jpeg?auto=compress&cs=tinysrgb&w=1200",
 "Woven Placemats (Set of 4)":"https://images.pexels.com/photos/13923045/pexels-photo-13923045.jpeg?auto=compress&cs=tinysrgb&w=1200"
 });
