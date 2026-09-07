8cf7f5885a4c043d36e3b83be8b98cb2abb6d908
rafoolee84 <albazirafal859@gmail.com>
2026-09-07 01:04:52 -0400

    Lighting type-match heroes: candle warmer, ceramic wall light, frosted night light

--- product-images-fixes.js
+++ product-images-fixes.js
@@ -3,5 +3,8 @@ window.NORTHROOM_IMAGES=Object.assign(window.NORTHROOM_IMAGES||{},{
 "Heavyweight Linen Pillowcases (Pair)":"https://images.pexels.com/photos/8325261/pexels-photo-8325261.jpeg?auto=compress&cs=tinysrgb&w=1200",
 "Linen Duvet Sham (Pair)":"https://images.unsplash.com/photo-1613721531731-3b7a7d96cb8a?w=1200&auto=format&fit=crop",
 "Textured Bath Mat":"https://images.pexels.com/photos/19203526/pexels-photo-19203526.jpeg?auto=compress&cs=tinysrgb&w=1200",
-"Woven Placemats (Set of 4)":"https://images.pexels.com/photos/13923045/pexels-photo-13923045.jpeg?auto=compress&cs=tinysrgb&w=1200"
+"Woven Placemats (Set of 4)":"https://images.pexels.com/photos/13923045/pexels-photo-13923045.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"Matte Black Candle Warmer":"https://images.pexels.com/photos/31236093/pexels-photo-31236093.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"Ceramic Wall Light Fixture":"https://images.pexels.com/photos/18545884/pexels-photo-18545884.jpeg?auto=compress&cs=tinysrgb&w=1200",
+"Frosted Glass Night Light":"https://images.pexels.com/photos/14380620/pexels-photo-14380620.jpeg?auto=compress&cs=tinysrgb&w=1200"
 });
