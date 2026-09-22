3f27a24f13834a24fba5d8cc3891eadd523f1087
Rafal Albazi <albazirafal859@gmail.com>
2026-09-22 10:06:33 -0400

    Fix: embed demoted SKUs in catalog-extra (CF 404 cache)

--- catalog-extra.js
+++ catalog-extra.js
@@ -46,3 +46,5 @@ window.NORTHROOM_BRAND_PRICE_OVERRIDES={};
     if (!c.image && imgs2[c.id]) c.image = imgs2[c.id];
   });
 })();
+
+window.NORTHROOM_DEMOTED_SKUS=window.NORTHROOM_DEMOTED_SKUS||["L-005","L-009","L-015","O-017","O-025","S-020","S-022","S-030","S-042","S-045","S-049","L-002","L-017","L-018"];
